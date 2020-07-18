f7c97bdf1e6b581acd5716e45a9b8a8e3b541ea7
muhammeta7 <muhammeta7@gmail.com>
2020-07-18 15:40:45 -0400

    Add-product component added along with DAO model.

---
+++ src/app/add-product/model/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  id: number;
+  name: string;
+  abbreviation: string;
+  location: string;
+  length: number;
+  width: number;
+  depth: number;
+  qty: number;
+  description: string;
+  category: string;
+}
