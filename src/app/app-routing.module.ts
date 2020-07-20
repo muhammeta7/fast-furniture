28cc2bea24821cd79247a320590e4a2fefcdd3f8
muhammeta7 <muhammeta7@gmail.com>
2020-07-20 16:42:32 -0400

    Add http method to get all products. Injected service into Inventory component to retrieve info from db.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AddProductComponent} from './add-product/add-product.component';
+import {InventoryComponent} from './inventory/inventory.component';
 
 
 const routes: Routes = [
-  {path: '', component: AddProductComponent}
+  {path: '', component: AddProductComponent},
+  {path: 'products', component: InventoryComponent}
 ];
 
 @NgModule({
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Product} from '../add-product/model/product';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private BASE_URL = 'http://localhost:8080/api/products';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient){}
+
+  getProducts(): Observable<Product[]>{
+    return this.httpClient.get<Product[]>(this.BASE_URL);
+  }
+
+
+
 }
