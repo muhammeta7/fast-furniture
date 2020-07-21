48697993455bea83f8863314844a949f00b7c292
muhammeta7 <muhammeta7@gmail.com>
2020-07-21 19:49:06 -0400

    Add form data to create product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {AddProductComponent} from './add-product/add-product.component';
 import {NavigationComponent} from './navigation/navigation.component';
 import {InventoryComponent} from './inventory/inventory.component';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
     declarations: [
@@ -18,7 +19,8 @@ import {HttpClientModule} from '@angular/common/http';
     imports: [
         BrowserModule,
         AppRoutingModule,
-        HttpClientModule
+        HttpClientModule,
+        FormsModule
     ],
     providers: [],
     bootstrap: [AppComponent]
