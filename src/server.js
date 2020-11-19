4f7b58085c43ffed4aed62c862a2be2e6bb1ce63
muhammeta7 <muhammeta7@gmail.com>
2020-11-19 05:16:28 -0500

    Update start method to node in package.json

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname + "/dist/fast-furniture-client"));
 
 app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname + "/dist/chatter/index.html"));
+    res.sendFile(path.join(__dirname + "/dist/fast-furniture-client/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
